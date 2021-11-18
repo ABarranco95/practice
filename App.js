17956fe94c6b6cf4132b503b5087564c5899708d
Angel <abarranco650@gmail.com>
2021-11-18 11:43:55 -0800

    installed bottom tab navigator

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import WorldClock from "./screens/WorldClock";
+import Alarm from "./screens/Alarm";
+import Stopwatch from "./screens/Stopwatch";
+import Timer from "./screens/Timer";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator initialRouteName="WorldClock">
+      <Tab.Screen name="WorldClock" component={WorldClock}></Tab.Screen>
+      <Tab.Screen name="Alarm" component={Alarm}></Tab.Screen>
+      <Tab.Screen name="Stopwatch" component={Stopwatch}></Tab.Screen>
+      <Tab.Screen name="Timer" component={Timer}></Tab.Screen>
+    </Tab.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/WorldClock.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const WorldClock = () => {
+  return (
+    <View>
+      <Text>This is my starting screen</Text>
+    </View>
+  );
+};
+
+export default WorldClock;
